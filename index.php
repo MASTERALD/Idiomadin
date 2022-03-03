9d8caf7205f4e02268291b04328232eb7dcf5d42
MASTERALD <93801083+MASTERALD@users.noreply.github.com>
2022-03-03 13:38:34 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FreeMovies</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<link rel="shortcut icon" href="logo_200x200_preview-rev-1.png" type="image/x-icon">
+</head>
+<body  style="background-color:dark";>
+
+    <form method="post">
+	<img class="img" src="logo_200x200_preview_rev_1.png">
+    	<h1>Registrate a Indiomadin</h1>
+    	<input type="text" name="name" placeholder="Nombre completo">
+    	<input type="email" name="email" placeholder="Email">
+    	<input type="submit" name="register">
+    </form>
+        <?php 
+        include("registrar.php");
+        ?>
+
+         
+</body>
+</html>
